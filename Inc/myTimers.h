4250e7ade91b43b40521b6fa43411ecb95079a9b
Nicolas7474 <nico7474@yahoo.fr>
2026-04-14 22:24:49 +0200

    Updated files organization

--- Inc/myTimers.h
+++ Inc/myTimers.h
@@ -5,27 +5,12 @@
 #include <stm32f469xx.h>
 
 void SysTick_Init(void);
-void SysTickDelayMs(int delay);
-
-void TIM2_Init (void);
-
-void TIM3config(uint16_t Timeout);
-
-void PWM_TIM5_Init(void);
-
-void TIM6_Init (uint16_t ms);
-
-void Delay_us_TIM7(uint16_t us);
-
-void TIM8_Init(void);
 
 void NBdelay_ms(uint32_t ms);
 
 extern volatile uint32_t msTicks;
 
 extern unsigned int countWakeUp;
-extern int flagmsTicks;  // extern
-
 
 #endif /* SYS_TICK_H_ */
 
